5ed92b1311202657b2e9e965641c1cf8902b1155
Themolx <themolxyt@gmail.com>
2024-10-03 22:35:18 +0200

    Version 3.2.
    i tried to replicate native nuke Ctrl+click feature in nuke.
    Now you can press Ctrl+E to select all nodes upstream.
    Ctrl+Alt+E to select all nodes in current tree.

--- NukeGrabTool.py
+++ NukeGrabTool.py
@@ -1,11 +1,13 @@
-# Nuke Advanced Grab Tool v3.0
+# Nuke Advanced Grab Tool v3.2
 #
-# This script implements an advanced grab tool with multiple modes for moving nodes in Nuke.
+# This script implements an advanced grab tool to mimic Nuke's native node movement behavior.
 #
 # Features:
 # - Standard Grab (E): Moves only selected nodes.
 # - Input Tree Grab (Cmd+Option+E): Moves the selected node and all its upstream nodes.
 # - Full Tree Grab (Cmd+E): Moves the entire connected node tree (upstream and downstream).
+# - Exit grab mode by pressing 'E' again
+# - Option to keep nodes selected after exiting grab mode (controlled by user variable)
 #
 # Usage:
 # 1. Select a node or nodes in Nuke
@@ -13,12 +15,16 @@
 # 3. Press 'Cmd+Option+E' to move the selected node and all its inputs
 # 4. Press 'Cmd+E' to move the entire connected node tree
 # 5. Move the mouse to reposition the nodes
-# 6. Left-click or press 'Enter' to confirm the new position
+# 6. Left-click, press 'Enter', or press 'E' again to confirm the new position
 # 7. Press 'Esc' to cancel the operation
 # 8. Press 'Z' to lock movement to X-axis, 'Y' to lock movement to Y-axis
 
 import nuke
 from PySide2 import QtCore, QtGui, QtWidgets
+
+# User variable to control whether nodes remain selected after grab mode
+# Note: Disabling this may be better for non-commercial versions of Nuke
+KEEP_NODES_SELECTED = True
 
 class AdvancedGrabTool(QtCore.QObject):
     def __init__(self):
@@ -35,7 +41,7 @@ class AdvancedGrabTool(QtCore.QObject):
         self.lock_y = False
         self.grab_mode = "standard"
 
-    def get_upstream_nodes(self, node, upstream=None):
+    def get_input_tree(self, node, upstream=None):
         if upstream is None:
             upstream = set()
         if node not in upstream:
@@ -43,18 +49,26 @@ class AdvancedGrabTool(QtCore.QObject):
             for i in range(node.inputs()):
                 input_node = node.input(i)
                 if input_node:
-                    self.get_upstream_nodes(input_node, upstream)
+                    self.get_input_tree(input_node, upstream)
         return upstream
 
-    def get_connected_nodes(self, start_node, connected=None):
-        if connected is None:
-            connected = set()
-        if start_node not in connected:
-            connected.add(start_node)
-            for n in start_node.dependencies():
-                self.get_connected_nodes(n, connected)
-            for n in start_node.dependent():
-                self.get_connected_nodes(n, connected)
+    def get_connected_nodes(self, start_node):
+        connected = set()
+        to_process = [start_node]
+        
+        while to_process:
+            node = to_process.pop(0)
+            if node not in connected:
+                connected.add(node)
+                
+                # Add all inputs (dependencies)
+                inputs = node.dependencies(nuke.INPUTS | nuke.HIDDEN_INPUTS)
+                to_process.extend([n for n in inputs if n not in connected])
+                
+                # Add all outputs (dependents)
+                outputs = node.dependent(nuke.INPUTS | nuke.HIDDEN_INPUTS)
+                to_process.extend([n for n in outputs if n not in connected])
+        
         return connected
 
     def activate_grab(self, mode="standard"):
@@ -71,9 +85,13 @@ class AdvancedGrabTool(QtCore.QObject):
         self.grab_mode = mode
 
         if self.grab_mode == "input_tree":
-            self.affected_nodes = self.get_upstream_nodes(self.selected_nodes[0])
+            self.affected_nodes = set()
+            for node in self.selected_nodes:
+                self.affected_nodes.update(self.get_input_tree(node))
         elif self.grab_mode == "full_tree":
-            self.affected_nodes = self.get_connected_nodes(self.selected_nodes[0])
+            self.affected_nodes = set()
+            for node in self.selected_nodes:
+                self.affected_nodes.update(self.get_connected_nodes(node))
         else:  # standard mode
             self.affected_nodes = set(self.selected_nodes)
 
@@ -94,7 +112,6 @@ class AdvancedGrabTool(QtCore.QObject):
         self.lock_x = False
         self.lock_y = False
         self.grab_mode = "standard"
-        self.affected_nodes.clear()
         
         app = QtWidgets.QApplication.instance()
         while app.overrideCursor() is not None:
@@ -104,6 +121,13 @@ class AdvancedGrabTool(QtCore.QObject):
             app.setOverrideCursor(self.original_cursor)
         
         QtWidgets.QApplication.instance().removeEventFilter(self)
+
+        # Keep nodes selected if the user variable is set to True
+        if not KEEP_NODES_SELECTED:
+            for node in self.affected_nodes:
+                node.setSelected(False)
+        
+        self.affected_nodes.clear()
 
     def apply_grab(self):
         self.deactivate_grab()
@@ -136,6 +160,9 @@ class AdvancedGrabTool(QtCore.QObject):
                 elif event.key() == QtCore.Qt.Key_Escape:
                     self.cancel_grab()
                     return True
+                elif event.key() == QtCore.Qt.Key_E:
+                    self.apply_grab()
+                    return True
         return False
 
     def update_positions(self, current_pos):
@@ -154,7 +181,10 @@ class AdvancedGrabTool(QtCore.QObject):
 grab_tool = AdvancedGrabTool()
 
 def grab_standard():
-    grab_tool.activate_grab(mode="standard")
+    if grab_tool.grab_active:
+        grab_tool.apply_grab()
+    else:
+        grab_tool.activate_grab(mode="standard")
 
 def grab_input_tree():
     grab_tool.activate_grab(mode="input_tree")
@@ -164,5 +194,5 @@ def grab_full_tree():
 
 # Add the Grab tool commands to Nuke's menu
 nuke.menu('Nuke').addCommand('Edit/Grab Tool', grab_standard, 'e')
-nuke.menu('Nuke').addCommand('Edit/Grab Input Tree', grab_input_tree, 'ctrl+e')
-nuke.menu('Nuke').addCommand('Edit/Grab Full Tree', grab_full_tree, 'alt+ctrl+e')
+nuke.menu('Nuke').addCommand('Edit/Grab Input Tree', grab_input_tree, 'alt+ctrl+e')
+nuke.menu('Nuke').addCommand('Edit/Grab Full Tree', grab_full_tree, 'ctrl+e')
